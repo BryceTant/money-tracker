6b6edb7d881096be03559cae96e29a3142036065
Bryce <BryceTant@gmail.com>
2021-08-28 22:47:58 -0400

    Final commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import './App.css'
 import { Header } from './components/Header'
+import { Balance } from './components/Balance'
+import { IncomeExpenses } from './components/IncomeExpenses'
+import { Transactions } from './components/Transactions'
+import { AddTransaction } from './components/AddTransaction'
+import { GlobalProvider } from './context/GlobalState'
 
 function App() {
   return (
-    <div>
+    <GlobalProvider>
       <Header />
-    </div>
+      <div className="container">
+        <Balance />
+        <IncomeExpenses />
+        <Transactions />
+        <AddTransaction />
+      </div>
+    </GlobalProvider>
   );
 }
 
